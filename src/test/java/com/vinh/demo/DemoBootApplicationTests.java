437e9c45ed9563f581209d525d231b6621ca5e02
vinh8888 <113347854+vinh8888@users.noreply.github.com>
2024-10-19 11:48:29 +0700

    Update DemoBootApplicationTests.java

--- src/test/java/com/vinh/demo/DemoBootApplicationTests.java
+++ src/test/java/com/vinh/demo/DemoBootApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoBootApplicationTests {
 
-	/*@Test
+	
 	void contextLoads() {
-	}*/
+		System.out.println("a";
+	}
 
 }
