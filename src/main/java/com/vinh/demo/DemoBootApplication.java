7b381c627af7934eb94273b93907b5269110fc83
vinh8888 <113347854+vinh8888@users.noreply.github.com>
2024-10-19 11:40:05 +0700

    Update DemoBootApplication.java

--- src/main/java/com/vinh/demo/DemoBootApplication.java
+++ src/main/java/com/vinh/demo/DemoBootApplication.java
@@ -2,13 +2,13 @@ package com.vinh.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
 
 @SpringBootApplication
 public class DemoBootApplication {
 
 	public static void main(String[] args) {
-		system
-		SpringApplication.run(DemoBootApplication.class, args);
+		SpringApplication.run(DemoBootApplication.class, args;
 	}
 
 }
