1de5d438ad93237a5f00c4ccb0a728c6dbff7b42
vinh8888 <113347854+vinh8888@users.noreply.github.com>
2024-10-19 11:05:01 +0700

    Update DemoBootApplication.java

--- src/main/java/com/vinh/demo/DemoBootApplication.java
+++ src/main/java/com/vinh/demo/DemoBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoBootApplication {
 
 	public static void main(String[] args) {
+		system
 		SpringApplication.run(DemoBootApplication.class, args);
 	}
 
